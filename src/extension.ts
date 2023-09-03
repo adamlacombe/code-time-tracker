7f318d406ee2daf5cb531c075449d7eb2bfed5aa
Adam LaCombe <git@adamlacombe.com>
2023-09-03 17:23:46 +0000

    Refactor activation and configuration handling

--- src/extension.ts
+++ src/extension.ts
@@ -6,29 +6,27 @@ import { StateOperations } from './state-manager';
 import { TimerOperations } from './timer';
 
 export function activate(context: vscode.ExtensionContext) {
-  const workspacePath = vscode.workspace.workspaceFolders![0].uri.path;
-  const gitPath = path.join(workspacePath, ".git");
+  const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
 
-  if (!gitPath) {
+  if (!workspaceFolder) {
     vscode.window.showErrorMessage("No workspace is opened.");
     return;
   }
 
-  const outputChannel = vscode.window.createOutputChannel(
-    "code-time-tracker",
-    "code-time-tracker-output",
-  );
+  const workspacePath = workspaceFolder.uri.path;
+  const gitPath = path.join(workspacePath, ".git");
+
+  const outputChannel = vscode.window.createOutputChannel("code-time-tracker");
   context.subscriptions.push(outputChannel);
   outputChannel.appendLine("Extension activated.");
 
-  let timerState: TimerState[] = context.workspaceState.get<TimerState[]>('timerState', []);
+  let timerState: TimerState[] = context.workspaceState.get<TimerState[]>("timerState", []);
   let elapsedSeconds = timerState.length ? timerState[0].seconds : 0;
 
-  const inactivityTimeoutSeconds = vscode.workspace.getConfiguration('timer').get<number>('inactivityTimeoutSeconds');
-  outputChannel.appendLine(`Setting inactivityTimeoutSeconds: ${inactivityTimeoutSeconds}`);
+  const inactivityTimeoutSeconds = vscode.workspace.getConfiguration("timer").get<number>("inactivityTimeoutSeconds") || 60;
 
   const gitOps = new GitOperations(gitPath);
-  const timerOps = new TimerOperations(inactivityTimeoutSeconds || 60, elapsedSeconds, timerState);
+  const timerOps = new TimerOperations(inactivityTimeoutSeconds, elapsedSeconds, timerState);
   const stateOps = new StateOperations(context, timerState);
 
   const timerStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
@@ -39,30 +37,22 @@ export function activate(context: vscode.ExtensionContext) {
   timerOps.startTimer(context, timerStatusBarItem);
 
   context.subscriptions.push(vscode.commands.registerCommand("code-time-tracker.pauseTimer", () => {
-    outputChannel.appendLine(`Pausing timer.`);
     vscode.window.showInformationMessage('Pausing timer.');
     timerOps.pauseTimer();
   }));
 
   context.subscriptions.push(vscode.commands.registerCommand("code-time-tracker.clearHistory", () => {
-    outputChannel.appendLine(`Clearing history.`);
     const state = timerOps.clearHistory();
     stateOps.setTimerState(state);
-    vscode.commands.executeCommand('code-time-tracker.showData');
+    vscode.commands.executeCommand("code-time-tracker.showData");
   }));
 
-  // Register the showData command
   registerShowDataCommand(context);
 
-  // File watchers and event handlers
   vscode.workspace.onDidChangeTextDocument((event) => {
-    if (event.document.uri.scheme !== 'file') {
+    if (event.document.uri.scheme !== "file") {
       return;
     }
-
-    outputChannel.appendLine(`File changed: ${event.document.fileName}`);
-    outputChannel.appendLine(`Uri scheme: ${event.document.uri.scheme}`);
-    outputChannel.appendLine(`Time: ${new Intl.DateTimeFormat('en-US', { hour: 'numeric', minute: 'numeric', hour12: true, second: 'numeric' }).format(new Date())}`);
 
     timerOps.resetInactivityTimeout(context, timerStatusBarItem);
     timerOps.updateFileSeconds(event.document.fileName);
@@ -74,10 +64,10 @@ export function activate(context: vscode.ExtensionContext) {
   });
 
   vscode.workspace.onDidChangeConfiguration((e) => {
-    if (e.affectsConfiguration('timer')) {
-      const inactivityTimeoutSeconds = vscode.workspace.getConfiguration('timer').get<number>('inactivityTimeoutSeconds');
+    if (e.affectsConfiguration("timer")) {
+      const inactivityTimeoutSeconds = vscode.workspace.getConfiguration("timer").get<number>("inactivityTimeoutSeconds") || 60;
       outputChannel.appendLine(`Updated inactivityTimeoutSeconds: ${inactivityTimeoutSeconds}`);
-      timerOps.setInactivityTimeoutSeconds(inactivityTimeoutSeconds || 60);
+      timerOps.setInactivityTimeoutSeconds(inactivityTimeoutSeconds);
     }
   });
 
@@ -100,7 +90,7 @@ export function activate(context: vscode.ExtensionContext) {
       }
 
       stateOps.updateAndSaveTimerState({ seconds: 0, files: [] });
-      timerOps.resetTimer(context, timerStatusBarItem);  // Resetting the timer
+      timerOps.resetTimer(context, timerStatusBarItem);
     }
   }, 30 * 1000);
 }
